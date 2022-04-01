ef0153a9e9435d4e003b4b8bc9e05523e12c7b48
harbouli <mharbouli@icloud.com>
2022-04-01 15:21:58 +0000

    FixUp

---
+++ src/translate/Fr.js
@@ -0,0 +1,44 @@
+export default {
+  //homme
+  homescrean: 'La Maintenance avec efficience et réactivité',
+
+  Plmo: 'Plomberie',
+  Elec: ' Électricité',
+  Manui: 'Menuiserie',
+  clima: 'Climatisation',
+
+  //AUTH
+
+  title2: 'Création de compte',
+
+  title3: 'Connexion',
+
+  FullName: 'Nom Complet',
+
+  Tel: 'Téléphone',
+  Setting: 'Changer de langue',
+
+  passWprd: 'Mot de passe',
+  address: 'Addresse',
+  Info: 'personal information',
+
+  Save: 'Enregistrer',
+  BTN1: 'Créer un compte',
+  Email: 'Address Mail',
+  Camera: 'Appareil photo ',
+  galerie: 'galerie',
+  imageProb: 'Image de problème',
+  chosePic: 'Choisissez une photo',
+
+  BTN2: 'Connexion',
+  btn4: 'Suivant',
+  LOGOUT: 'Déconnecter',
+
+  // PROB
+  title1: ' Réparons votre probléme',
+
+  Prob: 'Merci de décriver votre probléme',
+
+  btn3: 'Envoyer',
+  SendSc: 'Bien envoyé',
+};
